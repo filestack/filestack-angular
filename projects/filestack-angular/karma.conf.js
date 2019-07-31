e34d9c477fcf9626ae4b8f358c4e5098748d3c37
AndrzejSala <andrzej@filestack.com>
2019-07-31 16:28:14 +0200

    chore(karma): Update karma config

--- projects/filestack-angular/karma.conf.js
+++ projects/filestack-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
