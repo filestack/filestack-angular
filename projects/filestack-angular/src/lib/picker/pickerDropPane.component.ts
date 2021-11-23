0eadc0208d554064d0b1706aaf295b95adeb540d
Ravi Anand Kumar <ravianand1988@live.com>
2021-11-23 09:17:05 +0100

    fix: Picker base component naming

--- projects/filestack-angular/src/lib/picker/pickerDropPane.component.ts
+++ projects/filestack-angular/src/lib/picker/pickerDropPane.component.ts
@@ -1,26 +1,32 @@
-import { Component, ChangeDetectionStrategy, AfterContentInit } from '@angular/core';
-import { PickerBaseComponent } from './pickerBase.component';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+
 import { PickerDisplayMode } from 'filestack-js';
 
-@Component({
-    selector: 'ng-picker-drop-pane',
-    template: '<div [id]="elementId" class="ng-picker"></div>',
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class PickerDropPaneComponent extends PickerBaseComponent implements AfterContentInit {
-    ngAfterContentInit() {
-      // Overwrite display mode to be always 'dropPane' in this component
-      this.picker = this.filestackService.picker({
-        ...this.pickerOptions,
-        rootId: `picker-overlay-${Date.now()}`,
-        displayMode: PickerDisplayMode.dropPane,
-        container: this.elementId,
-        onUploadDone: res => this.uploadSuccess.next(res)
-      });
+import { PickerBaseDirective } from './pickerBase.component';
 
-      // Picker open success handler there is ommited, because it's accessible from pickerOptions
-      this.picker.open()
-        .then(void(0))
-        .catch(err => this.uploadError.next(err));
-    }
+@Component({
+  selector: 'ng-picker-drop-pane',
+  template: '<div [id]="elementId" class="ng-picker"></div>',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PickerDropPaneComponent extends PickerBaseDirective implements AfterContentInit {
+  ngAfterContentInit() {
+    // Overwrite display mode to be always 'dropPane' in this component
+    this.picker = this.filestackService.picker({
+      ...this.pickerOptions,
+      rootId: `picker-overlay-${Date.now()}`,
+      displayMode: PickerDisplayMode.dropPane,
+      container: this.elementId,
+      onUploadDone: res => this.uploadSuccess.next(res)
+    });
+
+    // Picker open success handler there is ommited, because it's accessible from pickerOptions
+    this.picker.open()
+      .then(void (0))
+      .catch(err => this.uploadError.next(err));
+  }
 }
